5c729f5abd52797fe078f6546c9e10cd044d794a
Zhanwei Liu <84030499+lzwei888@users.noreply.github.com>
2024-04-22 19:26:59 +0100

    Add files via upload

---
+++ ELE8088_Lab/(iii)battery.py
@@ -0,0 +1,148 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Ts = 0.1
+
+C = np.array([[1, 0, 0, 0, 0, 0],
+              [1, 0, 0, 0, 1, 0],
+              [1, 0, 0, 0, 0, 1]])
+
+G = 0.01 * np.eye(6)
+Q = 0.01 * np.eye(6)
+R = 0.01 * np.eye(3)
+
+
+def matrix_a(tau):
+    A = np.array(
+        [[1, Ts, 0, 0, 0, 0],
+         [0, 1, Ts, Ts * tau, 0, 0],
+         [0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0],
+         [0, 0, 0, 0, 1, 0],
+         [0, 0, 0, 0, 0, 1]]
+    )
+    return A
+
+
+def dynamics(x, tau):
+    A = matrix_a(tau)
+    w = np.random.multivariate_normal(np.zeros(6), Q).reshape(-1, 1)
+    x_next = A @ x + w
+    return x_next
+
+
+def output(x):
+    vt = np.random.multivariate_normal(np.zeros(3), R).reshape(-1, 1)
+    y = C @ x + vt
+    return y
+
+
+def measurement_update(x, sigma, y):
+    lhs = C @ sigma @ (C.T) + R
+    rhs = y - C @ x
+    x_meas = x + sigma @ (C.T) @ np.linalg.solve(lhs, rhs)
+
+    rhs1 = C @ sigma
+    sigma_meas = sigma - sigma @ (C.T) @ np.linalg.solve(lhs, rhs1)
+    return x_meas, sigma_meas
+
+
+def time_update(x_meas, sigma_meas, tau):
+    A = matrix_a(tau)
+    x_time = A @ x_meas
+    sigma_time = A @ sigma_meas @ (A.T) + G @ Q @ (G.T)
+    return x_time, sigma_time
+
+
+def height_control(arr, tau):
+    print(arr[-1], arr[-2])
+    if arr[-1] > 0.01 + arr[-2]:
+        tau -= 0.1
+        print("b")
+        if arr[-1] > 0.03 + arr[-2]:
+            tau -= 0.2
+        if arr[-1] > 0.3 + arr[-2]:
+            tau = 0.02
+    elif arr[-1] < -0.01 + arr[-2]:
+        tau += 0.1
+        print("s")
+        if arr[-1] < -0.03 + arr[-2]:
+            tau += 0.2
+        if arr[-1] < -0.3 + arr[-2]:
+            tau = 0.98
+
+    if tau <= 0:
+        tau = 0.1
+    elif tau >= 1:
+        tau = 0.99
+    return tau
+
+
+def height_control2(arr, tau):
+    if arr[-1] > 1.01 * arr[-2]:
+        tau -= 0.1
+        if arr[-1] > 1.05 * arr[-2]:
+            tau -= 0.25
+        if arr[-1] > 1.1 * arr[-2]:
+            tau = 0.02
+
+    elif arr[-1] < 0.99 * arr[-2]:
+        tau += 0.1
+        if arr[-1] < 0.95 * arr[-2]:
+            tau += 0.25
+        if arr[-1] < 0.9 * arr[-2]:
+            tau = 0.98
+
+    if tau <= 0:
+        tau = 0.01
+    elif tau >= 1:
+        tau = 0.99
+    return tau
+
+
+x_true = np.array([[10], [0.1], [-5], [10], [1], [1]])
+x_pred = np.array([[9], [0.1], [-6], [12], [1], [1]])
+sigma_pred = np.diag([1, 1, 2, 1, 1, 1])
+
+x_true_arr, x_pred_arr, x_meas_arr = [], [], []
+height = [10]
+N = 50
+tau = 0.5
+for t in range(N):
+    y = output(x_true)
+    x_meas, sigma_meas = measurement_update(x_pred, sigma_pred, y)
+    x_pred, sigma_pred = time_update(x_meas, sigma_meas, tau)
+    x_true = dynamics(x_true, tau)
+
+    height.append(x_true[0])
+    x_true_arr.append(x_true)
+    x_pred_arr.append(x_pred)
+    x_meas_arr.append(x_meas)
+
+    tau = height_control2(height, tau)
+
+    x_true[3] -= 0.6
+
+# plot
+x_plot = np.arange(N)
+y_plot, y_plot2, y_plot3 = [], [], []
+
+for i in range(len(x_true_arr)):
+    if x_true_arr[i][0] <= 0:
+        x_true_arr[i][0] = 0
+    y_plot.append(x_true_arr[i][0])
+    y_plot2.append(x_meas_arr[i][0])
+    # y_plot3.append(x_pred_arr[i][0])
+
+plt.plot(x_plot * 0.1, y_plot, label="x_true")
+plt.plot(x_plot * 0.1, y_plot2, label="x_meas")
+# plt.plot(x_plot, y_plot3, label="x_pred")
+plt.legend()
+plt.ylim(0, 15)
+plt.xlabel("Time (s)")
+plt.ylabel("Altitude (m)")
+plt.title("battery drains")
+plt.show()
+
+mse = np.mean((np.array(y_plot) - np.array(y_plot2)) ** 2)
+print("MSE =", mse)
---
+++ ELE8088_Lab/lab.py
@@ -0,0 +1,133 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def kalman_filter():
+    Ts = 0.1  # sampling rate
+
+    # matrix C
+    C = np.array([[1, 0, 0, 0, 0, 0],
+                  [1, 0, 0, 0, 1, 0],
+                  [1, 0, 0, 0, 0, 1]])
+
+    G = 0.1 * np.eye(6)
+    Q = 0.1 * np.eye(6)
+    R = 0.1 * np.eye(3)
+
+    # matrix A_t
+    def matrix_a(tau):
+        A = np.array(
+            [[1, Ts, 0, 0, 0, 0],
+             [0, 1, Ts, Ts * tau, 0, 0],
+             [0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 0, 0, 1]]
+        )
+        return A
+
+    # system dynamic
+    def dynamics(x, tau):
+        A = matrix_a(tau)
+        w = np.random.multivariate_normal(np.zeros(6), Q).reshape(-1, 1)
+        x_next = A @ x + w
+        return x_next
+
+    # system output
+    def output(x):
+        vt = np.random.multivariate_normal(np.zeros(3), R).reshape(-1, 1)
+        y = C @ x + vt
+        return y
+
+    def measurement_update(x, sigma, y):
+        lhs = C @ sigma @ (C.T) + R
+        rhs = y - C @ x
+        x_meas = x + sigma @ (C.T) @ np.linalg.solve(lhs, rhs)
+
+        rhs1 = C @ sigma
+        sigma_meas = sigma - sigma @ (C.T) @ np.linalg.solve(lhs, rhs1)
+        return x_meas, sigma_meas
+
+    def time_update(x_meas, sigma_meas, tau):
+        A = matrix_a(tau)
+        x_time = A @ x_meas
+        sigma_time = A @ sigma_meas @ (A.T) + G @ Q @ (G.T)
+        return x_time, sigma_time
+
+    # Advanced altitude control
+    def height_control2(arr, tau):
+        if arr[-1] > 1.01 * arr[-2]:
+            tau -= 0.1
+            if arr[-1] > 1.05 * arr[-2]:
+                tau -= 0.25
+            if arr[-1] > 1.1 * arr[-2]:
+                tau = 0.02
+        elif arr[-1] < 0.99 + arr[-2]:
+            tau += 0.1
+            if arr[-1] < 0.95 * arr[-2]:
+                tau += 0.25
+            if arr[-1] < 0.9 * arr[-2]:
+                tau = 0.98
+        if tau <= 0:
+            tau = 0.01
+        elif tau >= 1:
+            tau = 0.99
+        return tau
+
+    # x_true
+    x_true = np.array([[10], [0.1], [-0.5], [1], [1], [1]])
+    # initial predictions
+    x_pred = np.array([[9], [0.1], [-1], [2], [1], [1]])
+    sigma_pred = np.diag([1, 1, 2, 1, 1, 1])
+    # store the values after updates
+    x_true_arr, x_pred_arr, x_meas_arr = [], [], []
+    height = [10]
+    # 50 iterations, 5 seconds
+    N = 50
+    # initial value of tau
+    tau = 0.5
+    for t in range(N):
+        y = output(x_true)
+        x_meas, sigma_meas = measurement_update(x_pred, sigma_pred, y)
+        x_pred, sigma_pred = time_update(x_meas, sigma_meas, tau)
+        x_true = dynamics(x_true, tau)
+
+        height.append(x_true[0])
+        x_true_arr.append(x_true)
+        x_pred_arr.append(x_pred)
+        x_meas_arr.append(x_meas)
+
+        tau = height_control2(height, tau)
+        if x_true[0] <= 1:
+            tau = 1
+
+    # plot
+    x_plot = np.arange(N)
+    y_plot, y_plot2, y_plot3 = [], [], []
+
+    for i in range(len(x_true_arr)):
+        y_plot.append(x_true_arr[i][0])
+        y_plot2.append(x_meas_arr[i][0])
+        # y_plot3.append(x_pred_arr[i][0])
+
+    mse = np.mean((np.array(y_plot) - np.array(y_plot2)) ** 2)
+    if 12.5 >= x_true_arr[len(x_true_arr) - 1][0] >= 7.5:
+        plt.plot(x_plot * 0.1, y_plot, label="x_true")
+        plt.plot(x_plot * 0.1, y_plot2, label="x_meas")
+        # plt.plot(x_plot, y_plot3, label="x_pred")
+        plt.legend()
+        plt.ylim(0, 20)
+        plt.xlabel("Time (s)")
+        plt.ylabel("Altitude (m)")
+        plt.title("Kalman Filter")
+        plt.show()
+        print("MSE =", mse)
+        plot = True
+    else:
+        plot = False
+    return plot
+
+
+plot = False
+while plot == False:
+    plot = kalman_filter()
